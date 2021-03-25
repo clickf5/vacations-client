a528141bf1d0fb678fcfb0b7e497b078165e9701
clickf5 <paninhome@gmail.com>
2021-03-25 12:24:22 +0300

    feat: add routes API helper

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const host = '';
+const prefix = 'api/v1';
+
+export default {
+  usersPath: () => [host, prefix, 'users'].join('/'),
+  userPath: (id) => [host, prefix, 'user', id].join('/'),
+  signInPath: () => [host, prefix, 'signin'].join('/'),
+  signUpPath: () => [host, prefix, 'signup'].join('/'),
+  signOutPath: () => [host, prefix, 'signout'].join('/'),
+};
