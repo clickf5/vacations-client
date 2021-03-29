deabf5273774d471ddab594709e2f822b0cc079c
clickf5 <paninhome@gmail.com>
2021-03-29 14:37:18 +0300

    feat: add link to signup form

--- src/components/Signin/SignInForm.jsx
+++ src/components/Signin/SignInForm.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignInForm = () => (
   <div className="flex justify-center items-center min-h-screen">
@@ -44,11 +45,11 @@ const SignInForm = () => (
         </form>
       </div>
       <div className="flex items-center justify-center mt-6">
-        <a href="#" target="_blank" className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
+        <Link to="/signup" className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
           <span className="ml-2">
             You don&#x27;t have an account?
           </span>
-        </a>
+        </Link>
       </div>
     </div>
   </div>
