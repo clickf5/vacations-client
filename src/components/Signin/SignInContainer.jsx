b3f0a72dcd25ccb53ab02df793334025f126c236
clickf5 <paninhome@gmail.com>
2021-03-30 09:00:16 +0300

    feat: add formik to signin form

--- src/components/Signin/SignInContainer.jsx
+++ src/components/Signin/SignInContainer.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import SignInForm from './SignInForm.jsx';
 
-const SignInContainer = () => <SignInForm />;
+const SignInContainer = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <SignInForm
+      handleChange={formik.handleChange}
+      handleSubmit={formik.handleSubmit}
+      values={formik.values}
+    />
+  );
+};
 
 export default SignInContainer;
