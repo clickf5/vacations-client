63767deed333fd577f20764993ff8e089eb2bbef
clickf5 <paninhome@gmail.com>
2021-03-31 15:56:44 +0300

    feat: add withTimerRedirect hoc

---
+++ src/hoc/withTimerRedirect.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import withRedirect from './withRedirect.js';
+
+export default (ComposeComponent) => {
+  const withTimerRedirect = (props) => {
+    const {
+      seconds = 5,
+    } = props;
+
+    const [counter, setCounter] = useState(seconds);
+    const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+      const handler = () => {
+        setCounter((s) => {
+          if (s === 0) {
+            setRedirect(true);
+          }
+          return s - 1;
+        });
+      };
+
+      const timerId = setInterval(handler, 1000);
+      return () => clearInterval(timerId);
+    }, []);
+
+    const Component = withRedirect(ComposeComponent);
+    return <Component redirect={redirect} counter={counter} {...props} />;
+  };
+
+  return withTimerRedirect;
+};
