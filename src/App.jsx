fc234a1fcfbb83a707ed4af409033eb3e8cd649f
clickf5 <paninhome@gmail.com>
2021-04-02 16:44:42 +0300

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Redirect,
   Switch,
   Route,
 } from 'react-router-dom';
-import Main from './components/Main.jsx';
+import Dashboard from './components/Dasboard/Dashboard.jsx';
+import Users from './components/Users/Users.jsx';
 import SignInContainer from './components/Signin/SignInContainer.jsx';
 import SignUpContainer from './components/Signup/SignUpContainer.jsx';
 import NotFound from './components/NotFound.jsx';
@@ -14,10 +14,10 @@ const App = () => (
   <Router>
     <Switch>
       <Route exact path="/">
-        <Redirect to="/app" />
+        <Dashboard />
       </Route>
-      <Route path="/app">
-        <Main />
+      <Route path="/users">
+        <Users />
       </Route>
       <Route path="/signin">
         <SignInContainer />
