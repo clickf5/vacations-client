fb69c00af245938a3178cb55bb40c8a894179dde
Eugene Panin <paninhome@gmail.com>
2021-04-04 21:09:33 +0300

    fix: delete withSidebar

--- src/components/Dasboard/Dashboard.jsx
+++ src/components/Dasboard/Dashboard.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import withSidebar from '../../hoc/withSidebar.js';
 
 const Dashboard = () => (
   <div>Dashboard</div>
 );
 
-export default withSidebar(Dashboard);
+export default Dashboard;
