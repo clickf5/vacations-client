faf10041fb8983fe4a9414d04ddef0f50bedefd2
clickf5 <paninhome@gmail.com>
2021-04-12 16:51:43 +0300

    fix: delete filter

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -83,16 +83,6 @@ const Users = (props) => {
         <h2 className="text-2xl leading-tight">
           Users
         </h2>
-        <div className="text-end">
-          <form className="flex w-full max-w-sm space-x-3">
-            <div className=" relative ">
-              <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
-            </div>
-            <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-              Filter
-            </button>
-          </form>
-        </div>
       </div>
       <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
