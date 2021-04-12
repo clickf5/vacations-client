e8b496db050ba617e594b84964a19f41707822d3
clickf5 <paninhome@gmail.com>
2021-04-12 16:37:26 +0300

    feat: add ws event

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'regenerator-runtime/runtime';
+import { io } from 'socket.io-client';
 import { AuthProvider } from './hooks/useAuth';
 import App from './App.jsx';
 import getStore from './store';
+import { approveUser, dismissUser } from './slices/usersSlice.js';
 
 import './styles/style.scss';
 
 const store = getStore();
+
+const socket = io();
+
+socket.on('approve', ({ id }) => {
+  store.dispatch(approveUser(id));
+});
+
+socket.on('dismiss', ({ id }) => {
+  store.dispatch(dismissUser(id));
+});
 
 ReactDOM.render(
   <Provider store={store}>
