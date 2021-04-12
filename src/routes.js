2a97369bbbeeeadeca74f759fd63ced0aaf4fee0
clickf5 <paninhome@gmail.com>
2021-04-12 14:50:25 +0300

    feat: add approve and dismiss route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const prefix = 'api/v1';
 export default {
   usersPath: () => [host, prefix, 'users'].join('/'),
   userPath: (id) => [host, prefix, 'user', id].join('/'),
+  approvePath: (id) => [host, prefix, 'user', id, 'approve'].join('/'),
+  dismissPath: (id) => [host, prefix, 'user', id, 'dismiss'].join('/'),
   signInPath: () => [host, prefix, 'signin'].join('/'),
   signUpPath: () => [host, prefix, 'signup'].join('/'),
   signOutPath: () => [host, prefix, 'signout'].join('/'),
