282272c389522a701122a2633eedeb08a077a8ac
clickf5 <paninhome@gmail.com>
2021-04-12 16:37:04 +0300

    feat: add approveUser and dismissUser action

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -19,6 +19,32 @@ const usersSlice = createSlice({
   reducers: {
     addUser: (state, action) => [...state, action.payload],
     deleteUser: (state, action) => state.filter((user) => user.id !== action.payload),
+    approveUser: (state, action) => {
+      const { entities } = state;
+
+      const mappedEntities = entities.map((user) => {
+        const { _id: id } = user;
+        if (id === action.payload) {
+          return { ...user, approved: true };
+        }
+        return user;
+      });
+
+      return { ...state, entities: mappedEntities };
+    },
+    dismissUser: (state, action) => {
+      const { entities } = state;
+
+      const mappedEntities = entities.map((user) => {
+        const { _id: id } = user;
+        if (id === action.payload) {
+          return { ...user, approved: false };
+        }
+        return user;
+      });
+
+      return { ...state, entities: mappedEntities };
+    },
   },
   extraReducers: {
     [fetchUsers.pending]: (state) => {
@@ -31,6 +57,8 @@ const usersSlice = createSlice({
   },
 });
 
-export const { addUser, deleteUser } = usersSlice.actions;
+export const {
+  addUser, deleteUser, approveUser, dismissUser,
+} = usersSlice.actions;
 
 export default usersSlice.reducer;
