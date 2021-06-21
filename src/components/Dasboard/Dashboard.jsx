f946e8ebfd08e20167584b1c6cd31a0670890711
Eugene Panin <paninhome@gmail.com>
2021-06-21 10:36:37 +0300

    fix: dashboard layout

--- src/components/Dasboard/Dashboard.jsx
+++ src/components/Dasboard/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import AddVacationFormContainer from '../AddVacationForm/AddVacationFormContainer.jsx';
 
 const Dashboard = () => (
-  <div>Dashboard</div>
+  <header className="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
+    <div className="relative flex flex-col justify-center h-full px-3 mx-auto flex-center">
+      <div className="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
+        <div className="relative p-1 flex items-end justify-end w-full ml-5 mr-4 sm:mr-0 sm:right-auto">
+          <AddVacationFormContainer />
+        </div>
+      </div>
+    </div>
+  </header>
 );
 
 export default Dashboard;
